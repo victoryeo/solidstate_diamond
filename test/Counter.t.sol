66908b97124393d87623ab3d2c3e98f0f0bc37c8
victoryeo <victoryeo1974@gmail.com>
2024-11-07 15:56:05 +0800

    add upgrade test

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,23 +2,45 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
+import {IERC2535DiamondCutInternal} from "solidstate-solidity/interfaces/IERC2535DiamondCutInternal.sol";
 import {CounterFacet} from "../src/Counter/CounterFacet.sol";
+import {Counter} from "../src/Counter.sol";
 
 contract CounterTest is Test {
-    CounterFacet public counter;
+    Counter public counter;
+    CounterFacet public counterFacet;
 
     function setUp() public {
-        counter = new CounterFacet();
-        counter.setNumber(0);
+        counter = new Counter();
+        counterFacet = new CounterFacet();
+        counterFacet.setNumber(0);
     }
 
     function test_Increment() public {
-        counter.increment();
-        assertEq(counter.getNumber() , 1);
+        counterFacet.increment();
+        assertEq(counterFacet.getNumber() , 1);
     }
 
     function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.getNumber(), x);
+        counterFacet.setNumber(x);
+        assertEq(counterFacet.getNumber(), x);
     }
+
+    function test_upgrade() public {
+        IERC2535DiamondCutInternal.FacetCut[] memory facetCuts = new IERC2535DiamondCutInternal.FacetCut[](1);
+        bytes4[] memory selectors = new bytes4[](1);
+        uint256 selectorIndex;
+
+        // register a new function
+        selectors[selectorIndex++] = CounterFacet.getNumber.selector;
+
+        facetCuts[0] = IERC2535DiamondCutInternal.FacetCut({
+            target: address(this),
+            action: IERC2535DiamondCutInternal.FacetCutAction.ADD,
+            selectors: selectors
+        });
+
+        counter.diamondCut(facetCuts, address(0), "");
+    }
+
 }
